3b98c4ddddf6a7d3b19784bdddae41f247000321
Hyoyoung Lee <lhy2361@naver.com>
2022-07-14 10:12:54 +0900

    20220713 commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    /*const name = 'Josh Perez';*/
+    const user = {
+        firstName: 'Harper',
+        lastName: 'Perez'
+    };
+
+
+
+    /*const element = <h1> Hello, {name} </h1>*/
+    
+    // const element = <h1> Hello, {formatName(user)} </h1>
+    /* 위처럼 name과 같은 변수에도 중괄호, formatName과 같은 함수에도 중괄호! */
+
+    const element = <a href = "https://www.reactjs.org"> link </a>;
+
+
+    function formatName(user) {
+        return user.firstName + ' ' + user.lastName;
+    }
+
+    function getGreeting(user) {
+        if (user) {
+            return <h1> Hello, {formatName(user)} </h1>
+        } 
+            return <h1> stranger ~ </h1>
+        
+    }
+
+
+    return(
+        <>
+        {element}
+        {getGreeting(user)}
+        {/*여기에다가는 따로 중괄호를 씌우지 않는당~! */}
+        </>
+    )
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// React 엘리먼트를 루트 DOM 노드에 렌더링하기 위해.
+
 root.render(
   <React.StrictMode>
     <App />
@@ -14,4 +15,4 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
